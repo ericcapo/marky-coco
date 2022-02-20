bb0e8cc733389d2ed142521567d23847acd0b777
Eric Capo <eric.capo@hotmail.fr>
2022-02-20 13:38:53 +0100

    Delete marky.sh

--- marky.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-sample="$1"
-
-module load conda
-source conda_init.sh
-conda activate coco
-snakemake --cores 12 ${sample}_tmp/${sample}_megahit
-snakemake --cores 12 ${sample}_tmp/${sample}.index.1.bt2
-snakemake --cores 12 ${sample}_outputs/${sample}_hgcA_final.txt
