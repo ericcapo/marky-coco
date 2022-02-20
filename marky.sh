1a426993b9677605749290d781084da047a4c401
Eric Capo <eric.capo@hotmail.fr>
2022-02-20 13:39:04 +0100

    Add files via upload

---
+++ marky.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+sample="$1"
+
+snakemake --cores 12 ${sample}_tmp/${sample}_megahit
+snakemake --cores 12 ${sample}_tmp/${sample}.index.1.bt2
+snakemake --cores 12 ${sample}_outputs/${sample}_hgcA_final.txt
---
+++ marky_to_slurm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash				
+#SBATCH -A snic2021-5-53				
+#SBATCH -n 12				
+#SBATCH --time=10:00:00	
+
+sample="$1"
+
+module load conda
+source conda_init.sh
+conda activate coco
+snakemake --cores 12 ${sample}_tmp/${sample}_megahit
+snakemake --cores 12 ${sample}_tmp/${sample}.index.1.bt2
+snakemake --cores 12 ${sample}_outputs/${sample}_hgcA_final.txt
